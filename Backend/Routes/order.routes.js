8d0940797dc8e4284ff0e7232d20729d329bc115
AbeloByte <gobeztech20@gmail.com>
2025-03-25 23:59:45 +0300

    order route

--- Backend/Routes/order.routes.js
+++ Backend/Routes/order.routes.js
@@ -1,13 +1,18 @@
-// //
-// // import express
-// const express = require("express");
-// //  call the router method from the express
-// const router = express.Router();
+// import express
+const express = require("express");
+//  call the router method from the express
+const router = express.Router();
 
-// // import the order  controller
-// const orderController = require("../Controllers/order.controller");
+// import the order  controller
+const orderController = require("../Controllers/order.controller");
 
-// // create a route to handle the login req
-// router.post("/api/order", orderController.createOrder);
+// create a route to handle the login req
+router.post("/api/order", orderController.createNewOrder);
 
-// module.exports = router;
+// create a route to get all orders
+router.get("/api/orders", orderController.getAllOrders);
+
+// create a route to get a single order
+router.get("/api/order/:id", orderController.getSingleOrder);
+
+module.exports = router;
