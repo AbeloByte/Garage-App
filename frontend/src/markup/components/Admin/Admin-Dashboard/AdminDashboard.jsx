81e464c5d30d9c58559b05781639e8a403b48d96
AbeloByte <gobeztech20@gmail.com>
2025-04-14 23:50:45 +0300

    Modify Dashboard

--- frontend/src/markup/components/Admin/Admin-Dashboard/AdminDashboard.jsx
+++ frontend/src/markup/components/Admin/Admin-Dashboard/AdminDashboard.jsx
@@ -1,39 +1,51 @@
 import React from "react";
 import Card from "../Cards/Card";
+import turbocharger from "../../../../assets/images/icons/Turbocharger.png";
+import brake from "../../../../assets/images/Service Images/brake-disk.png";
+import autoTransmission from "../../../../assets/images/Service Images/automatic-transmission.png";
+import flatTire from "../../../../assets/images/Service Images/flat-tire.png";
+import sprayGun from "../../../../assets/images/Service Images/spray-gun.png";
+import carEngine from "../../../../assets/images/Service Images/car-engine.png";
 function AdminDashboard() {
   const dashboardData = [
     {
       title: "All Orders",
+      img: turbocharger,
       category: "OPEN FOR ALL",
       link: "#",
       linkText: "LIST OF ORDERS →",
     },
     {
       title: "New Orders",
+      img: autoTransmission,
       category: "OPEN FOR LEADS",
       link: "#",
       linkText: "ADD ORDER →",
     },
     {
       title: "Employees List",
+      img: brake,
       category: "OPEN FOR ADMINS",
       link: "#",
       linkText: "LIST OF EMPLOYEES →",
     },
     {
       title: "Add Employee",
+      img: flatTire,
       category: "OPEN FOR ADMINS",
       link: "#",
       linkText: "READ MORE →",
     },
     {
       title: "Engine Service & Repair",
+      img: carEngine,
       category: "SERVICE AND REPAIRS",
       link: "#",
       linkText: "READ MORE →",
     },
     {
       title: "Tyre & Wheels",
+      img: flatTire,
       category: "SERVICE AND REPAIRS",
       link: "#",
       linkText: "READ MORE →",
@@ -41,17 +53,20 @@ function AdminDashboard() {
     {
       title: "Denting & Painting",
       category: "SERVICE AND REPAIRS",
+      img: sprayGun,
       link: "#",
       linkText: "READ MORE →",
     },
     {
       title: "Denting & Painting",
+      img: sprayGun,
       category: "SERVICE AND REPAIRS",
       link: "#",
       linkText: "READ MORE →",
     },
     {
       title: "Denting & Painting",
+      img: sprayGun,
       category: "SERVICE AND REPAIRS",
       link: "#",
       linkText: "READ MORE →",
@@ -74,6 +89,7 @@ function AdminDashboard() {
               key={index}
               category={card.category}
               title={card.title}
+              img={card.img}
               link={card.link}
               linkText={card.linkText}
             />
